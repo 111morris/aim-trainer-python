b1154d90480d6d7080c1d279474521ce904e52a0
mulandi <mulandimorris1@gmail.com>
2025-06-30 14:43:40 +0300

    adding target class

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import random
 import pygame
 import time
 pygame.init()
+
 WIDTH, HEIGHT = 800, 600
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("Aim Trainer")
+
+class Target: 
+    MAX_SIZE = 30
+    GROWTH_RATE = 0.2
+    COLOR = 'red'
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.size = 0
 
 def main():
     run = True
