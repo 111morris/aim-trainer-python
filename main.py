951c7f568a29b74df9668e17837f083c42f2ef06
mulandi <mulandimorris1@gmail.com>
2025-06-30 15:08:54 +0300

    cleaning up

--- main.py
+++ main.py
@@ -14,11 +14,13 @@ class Target:
     MAX_SIZE = 30
     GROWTH_RATE = 0.2
     COLOR = 'red'
+    SECOND_COLOR = 'white'
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.size = 0
+        self.grow = True
 
     def update(self):
         if self.size + self.GROWTH_RATE >= self.MAX_SIZE:
@@ -28,8 +30,13 @@ class Target:
             self.size += self.GROWTH_RATE
         else: 
             self.size -= self.GROWTH_RATE
+
+
     def draw(self, win):
         pygame.draw.circle(win, self.COLOR, (self.x, self.y,), self.size)
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y,), self.size * 8)
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y,), self.size * 0.6)
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y,), self.size * 0.4)
 def main():
     run = True
     while run:
