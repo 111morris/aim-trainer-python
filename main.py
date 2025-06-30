533a1cede35de674b92e5bbbb3c7788d1ab35c09
mulandi <mulandimorris1@gmail.com>
2025-06-30 14:37:37 +0300

    add the main file and initialized it

---
+++ main.py
@@ -0,0 +1,11 @@
+import math 
+import random
+import pygame
+import time
+paygame.init()
+
+WIDTH, HEIGHT = 800, 600
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Aim Trainer")
